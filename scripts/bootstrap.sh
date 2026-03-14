6772aa0ff7ab96f6d9684241d0e883cf6ef841e3
Kevin Fernando <kevinchanaka1@gmail.com>
2026-03-14 13:46:26 +1100

    Add bootstrap script and ESO

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# This script bootstraps a new cluster
+
+# ArgoCD initial login password
+# kubectl get secret -n argocd argocd-initial-admin-secret -o=jsonpath={.data.password} | base64 --decode
+
+# ArgoCD initial port forward
+# kubectl port-forward -n argocd service/argocd-server 8080:443
+
+set -euo pipefail
+
+clientId=${INFISICAL_CLIENT_ID:?}
+clientSecret=${INFISICAL_CLIENT_SECRET:?}
+
+echo "Adding helm repos"
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add flannel https://flannel-io.github.io/flannel/
+
+echo "Templating manifests for flannel"
+helm template flannel -f values/flannel.yaml --namespace kube-system flannel/flannel | kubectl apply --server-side -f -
+sleep 10
+
+echo "Templating manifests for ArgoCD"
+kubectl create namespace argocd || true
+helm template argocd oci://ghcr.io/argoproj/argo-helm/argo-cd --create-namespace --namespace argocd | kubectl apply --server-side -f -
+sleep 10
+
+echo "Installing ESO secret"
+kubectl create namespace external-secrets || true
+kubectl create secret generic infisical \
+    --namespace external-secrets \
+    --from-literal=clientId=${clientId} \
+    --from-literal=clientSecret=${clientSecret} \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Deploying root app-of-apps"
+kubectl apply -f root.yaml
+
+echo "Bootstrap complete"
